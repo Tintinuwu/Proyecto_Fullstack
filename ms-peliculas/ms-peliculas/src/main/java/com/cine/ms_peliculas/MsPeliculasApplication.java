e5e2370cb0a51354593e629590a2d0abbd607cb4
Tomas <tom.delgado@duocuc.cl>
2026-05-22 13:38:26 -0400

    feat: agregar microservicios reservas, pagos, peliculas, usuarios

---
+++ ms-peliculas/ms-peliculas/src/main/java/com/cine/ms_peliculas/MsPeliculasApplication.java
@@ -0,0 +1,13 @@
+package com.cine.ms_peliculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPeliculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPeliculasApplication.class, args);
+	}
+
+}
---
+++ ms-peliculas/ms-peliculas/src/main/java/com/cine/ms_peliculas/repository/PeliculaRepository.java
@@ -0,0 +1,18 @@
+package com.cine.ms_peliculas.repository;
+
+import com.cine.ms_peliculas.model.Pelicula;
+import com.cine.ms_peliculas.model.Pelicula;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface PeliculaRepository extends JpaRepository<Pelicula, Long> {
+
+    // Estos métodos permiten a Spring Data JPA generar la consulta SQL automáticamente
+    boolean existsByTituloIgnoreCase(String titulo);
+
+    List<Pelicula> findByActivoTrue();
+
+    List<Pelicula> findByGeneroAndActivoTrue(String genero);
+}
